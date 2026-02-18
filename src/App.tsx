41db3912ecabd9df654f7932a337d48452eaae32
Yaelin <hough.yaelin@gmail.com>
2026-02-18 15:09:28 -0500

    feat: created new button, adjusted layout dimensions, colors, and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
+  const inputRef = useRef<HTMLTextAreaElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
   return (
     <div className="popup-container">
       <h1>InstaCal</h1>
       <h2 className="subheading">Plan your next event</h2>
       <textarea
+        ref={inputRef}
         className="event-input"
         placeholder="Dinner with Gabe this Monday at 6"
       />
+      <button className="add-event-btn">Add Event</button>
     </div>
   )
 }
