feee0ba922f5ead12f2194186fbc8a93063a57f5
Yaelin <hough.yaelin@gmail.com>
2026-02-18 14:47:11 -0500

    feat: updated UI with new styles and fonts, updated layout for event input

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="popup-container">
       <h1>InstaCal</h1>
-      <p>Extension is active</p>
+      <h2 className="subheading">Plan your next event</h2>
+      <textarea
+        className="event-input"
+        placeholder="Dinner with Gabe this Monday at 6"
+      />
     </div>
   )
 }
